05405733ced7968fdcb86583f84b169c70bd65ca
venjal <35352372+venjal@users.noreply.github.com>
2024-09-24 00:36:27 -0500

    Update adusersetup.ps1

--- adusersetup.ps1
+++ adusersetup.ps1
@@ -12,7 +12,7 @@ New-ADGroup -Name "Sales" -SamAccountName Sales `
     -GroupCategory Security `
     -GroupScope Global `
     -DisplayName "Sales Team" `
-    -Path "CN=Users,DC=fabricom,DC=local" `
+    -Path "CN=Users,DC=contoso,DC=com" `
     -Description "Members of the Sales Team"
 
 New-ADUser -Name "adsales" -Description "sales team member" -Enabled $true -AccountPassword $pw
